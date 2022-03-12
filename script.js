12e5eb611486279b990dfc420bebd78f8e08b4b4
HabiburRahmanAnik <habib264521@gmail.com>
2022-03-12 13:34:19 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+const btnLeft = document.querySelector('.btn-left');
+const btnRight = document.querySelector('.btn-left');
+const img = document.querySelector('#currImg');
+
+// const imgs = [
+//   'img/avenue-2215317_1280.jpg',
+//   'img/mountains-100367_1920.jpg',
+//   'img/nature-g492b774d4_1280.jpg',
+//   'img/night-3078326_1280.jpg',
+//   'img/sunset-1373171_1920.jpg',
+// ];
+
+let currentSlide = 0;
+// const tick = () => {
+//   if (currentSlide === imgs.length - 1) {
+//     currentSlide = 0;
+//   } else {
+//     currentSlide++;
+//   }
+//   img.setAttribute('src', imgs[currentSlide]);
+// };
+
+// setInterval(tick, 3000);
+// tick();
+btnLeft.addEventListener('click', function () {
+
+});
